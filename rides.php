ff63de77e6ede236ca10ff5f9f70ebc71c38a11c
aarish407 <aarish407@gmail.com>
2018-04-10 09:29:38 +0530

    Added all rides to rides.php

--- rides.php
+++ rides.php
@@ -260,9 +260,9 @@ session_start();
 
                             <div class="rides-list">
 
-                                <article class="ride-box clearfix">
+                                
+                                <?php                                    
 
-                                <?php
                                     include 'php/config.php';
 
                                     $query= "SELECT * FROM rides";
@@ -271,9 +271,10 @@ session_start();
                                     while($row= mysqli_fetch_array($data))
                                     {
                                         ?>
-                                        
+                                        <article class="ride-box clearfix">
+
                                         <div class="ride-content">
-                                            <h3><a href="#">From <?php echo $row['source'] ?> to <?php echo $row['destination'] ?></a></h3>ride by <a href="#"> <?php $row['name'] ?> </a>
+                                            <h3><a href="#">From <?php echo $row['source'] ?> to <?php echo $row['destination'] ?></a></h3>ride by <a href="#"> <?php echo $row['name'] ?> </a>
                                         </div>
 
                                         <ul class="ride-meta">
@@ -286,6 +287,7 @@ session_start();
                                             </li><!-- end .ride-date -->
                                         </ul><!-- end .ride-meta -->  
                                         
+                                        </article><!-- end .ride-box -->
                                         <?php
                                     }
                                 ?>
@@ -310,9 +312,9 @@ session_start();
 
                                     
 
-                                </article><!-- end .ride-box -->
+                                
 
-                                <article class="ride-box clearfix">
+                                <!-- <article class="ride-box clearfix">
 
                                     <div class="ride-content">
                                         <h3><a href="#">From Milano to Rome</a></h3>ride by <a href="#">Carlo Corti</a>
@@ -327,134 +329,8 @@ session_start();
                                             </a>
                                         </li><!-- end .ride-date -->
 
-                                        <li class="ride-people">
-                                            <a href="#" class="tooltip-link" data-original-title="Number of seats" data-toggle="tooltip">
-                                                <i class="fa fa-user"></i>
-                                                4
-                                            </a>
-                                        </li><!-- end .ride-people -->
-
-                                        <li>
-                                            <a href="#">
-                                                <i class="fa fa-file"></i>
-                                                Read more
-                                            </a>
-                                        </li>
-
-                                    </ul><!-- end .ride-meta -->
-
-                                </article><!-- end .ride-box -->
-
-                                <article class="ride-box clearfix">
-
-                                    <div class="ride-content">
-                                        <h3><a href="#">From Lyon to Paris</a></h3>ride by <a href="#">Antoine Bonnet</a>
-                                    </div>
-
-                                    <ul class="ride-meta">
-
-                                        <li class="ride-date">
-                                            <a href="#" class="tooltip-link" data-original-title="Date" data-toggle="tooltip">
-                                                <i class="fa fa-calendar"></i>
-                                                July 15, 2014 at 20:00 PM
-                                            </a>
-                                        </li><!-- end .ride-date -->
-
-                                        <li class="ride-people">
-                                            <a href="#" class="tooltip-link" data-original-title="Number of seats" data-toggle="tooltip">
-                                                <i class="fa fa-user"></i>
-                                                3
-                                            </a>
-                                        </li><!-- end .ride-people -->
-
-                                        <li>
-                                            <a href="#">
-                                                <i class="fa fa-file"></i>
-                                                Read more
-                                            </a>
-                                        </li>
-
-                                    </ul><!-- end .ride-meta -->
-
-                                </article><!-- end .ride-box -->
-
-                                <article class="ride-box clearfix">
-
-                                    <div class="ride-content">
-                                        <h3><a href="#">From Barcelona to Madrid</a></h3>ride by <a href="#">Adriana Bello</a>
-                                    </div>
-
-                                    <ul class="ride-meta">
-
-                                        <li class="ride-date">
-                                            <a href="#" class="tooltip-link" data-original-title="Date" data-toggle="tooltip">
-                                                <i class="fa fa-calendar"></i>
-                                                July 10, 2014 at 09:00 AM
-                                            </a>
-                                        </li><!-- end .ride-date -->
-
-                                        <li class="ride-people">
-                                            <a href="#" class="tooltip-link" data-original-title="Number of seats" data-toggle="tooltip">
-                                                <i class="fa fa-user"></i>
-                                                2
-                                            </a>
-                                        </li><!-- end .ride-people -->
-
-                                        <li>
-                                            <a href="#">
-                                                <i class="fa fa-file"></i>
-                                                Read more
-                                            </a>
-                                        </li>
-                                    </ul><!-- end .ride-meta -->
-
-                                </article><!-- end .ride-box -->
-
-                                <article class="ride-box clearfix">
-
-                                    <div class="ride-content">
-                                        <h3><a href="#">From Munich to Berlin</a></h3>ride by <a href="#">Albert Becker</a>
-                                    </div>
-
-                                    <ul class="ride-meta">
-
-                                        <li class="ride-date">
-                                            <a href="#" class="tooltip-link" data-original-title="Date" data-toggle="tooltip">
-                                                <i class="fa fa-calendar"></i>
-                                                July 08, 2014 at 22:00 PM
-                                            </a>
-                                        </li><!-- end .ride-date -->
-
-                                        <li class="ride-people">
-                                            <a href="#" class="tooltip-link" data-original-title="Number of seats" data-toggle="tooltip">
-                                                <i class="fa fa-user"></i>
-                                                1
-                                            </a>
-                                        </li><!-- end .ride-people -->
-
-                                        <li>
-                                            <a href="#">
-                                                <i class="fa fa-file"></i>
-                                                Read more
-                                            </a>
-                                        </li>
-
-                                    </ul><!-- end .ride-meta -->
-
-                                </article><!-- end .ride-box -->
-
+                                        
                                 <div class="clearfix"></div>
-
-                                <div class="post-pagination pagination-margin clearfix">
-
-                                    <div class="next pull-right">
-                                        <a href="#">
-                                            Next
-                                            <i class="fa fa-chevron-right"></i>
-                                        </a>
-                                    </div>
-
-                                </div><!-- end .post-pagination -->
 
                             </div><!-- end .events-list -->
 
